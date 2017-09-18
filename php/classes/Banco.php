280c3dc13398716e037c2207d50f19ddf77f5ba6
Anologicon <pauloraimundi2009@unifebe.edu.br>
2017-09-17 23:32:20 -0300

    function conectar and function criar_tabela

--- php/classes/Banco.php
+++ php/classes/Banco.php
@@ -1,43 +1,91 @@
 <?php
-
+/**
+ *   Obect 'Banco', cria o ambiente para rodar a aplicação, e as funções DML
+ */
 namespace classes;
 
 class Banco
 {
-  function __construct($base_dados = false, $tabela = false, $senha = false, $login = false)
+  function __construct($server = false,$login = false,$senha = false)
   {
-      $this->data_base = $base_dados;
-      $this->tabela = $tabela;
-      $this->senha = $senha;
-      $this->login = $login;
+    $this->connect_server = $server;
+    $this->senha = $senha;
+    $this->login = $login;
+
+    $this->sql = mysqli_connect($this->connect_server,$this->login,$this->senha);
   }
 
+/**
+  * Cria o ambiente para rodar a aplicação;
+  *
+  *  @author Anologicon
+  *
+  */
   public function create_banco()
   {
-    $base = $this->data_base;
+    //faz conexao com o banco
 
-    $sql = mysqli_connect('localhost','root','');
-    if ($sql->connect_error)
+    // Verifica se ouve algum erro de conexão
+    if ($this->sql->connect_error)
     {
-    die("Falha na conexao: " . $sql->connect_error);
+    die("Falha na conexao: " . $this->sql->connect_error);
     }
 
-    $data = $sql->query("SHOW DATABASES");
+    // Preparando query para mostrar todas as base de dados
+    $data = $this->sql->query("SHOW DATABASES");
+    /*
+    Para cada base de dados comparamos com a base 'default' da aplicação
+    Se for igual ele retorna true setando $bool_base
+    */
     while ($result = $data->fetch_row()) {
-        $result[0] == $base ? $bool_base = true : $bool_base = false;
+        $result[0] == 'teste' ? $bool_base = true : $bool_base = false;
     }
-
+    /*
+    Se $bool_base estiver true ele apaga a base de dados atual e cria uma
+    nova 'teste'
+    Se $bool_base estiver false ele cria uma base nova 'teste'
+    */
     if($bool_base){
-      $sql->query("DROP DATABASE".$base);
-      $sql->query("CREATE DATABASE ".$base);
+      $this->sql->query("DROP DATABASE teste");
+      $this->sql->query("CREATE DATABASE  teste");
     }else{
-      $sql->query("CREATE DATABASE".$base);
+      $this->sql->query("CREATE DATABASE teste");
     }
-      $sql->close();
+    $this->sql->close();
   }
 
-  public function conectar(){
-    
+  /**
+   * [conectar cria conexão com deafault padrão caso não receba o primeiro parametro]
+   * @param  [String] $_server [caminho do servidor]
+   * @param  [String] $_log    [login para o servidor]
+   * @param  [String] $_pass   [senha]
+   * @param  [String] $_base   [a base de dados]
+   * @return           [cria a conexão de dados ou usa o deafult]
+   */
+  public function conectar($_server = null, $_log = null, $_pass = null,$_base = null){
+    //se $_server é null, cria uma conexão default, se não ele cria com os parametros
+    if($_server == false){
+      $this->sql = mysqli_connect($this->connect_server,$this->login,$this->senha,'teste');
+    }else {
+      $this->sql = mysqli_connect($_server,$_log,$_pass,$_base);
+    }
+  }
+
+  /**
+   * [criar_tabela cria a tabela]
+   * @param  [String] $_tabela [nome da tabela que deseja criar]
+   *
+   */
+  public function criar_tabela($_tabela = null){
+    // se a $_tabela for null, cria como default tabela alunos e insere dados
+    // se não ele cria a tabela com o parametro passado
+    if($_tabela == false){
+      if($this->sql->query('CREATE TABLE IF NOT EXISTS Alunos (nome varchar(50),curso varchar(50),fase int,cpf varchar(50) PRIMARY KEY)')){
+            $this->sql->query('INSERT INTO alunos VALUES ("Maria","Informatica",null,"123.456.789-10")');
+      }
+    }else{
+      $this->sql->query('CRETE TABLE IF NOT EXISTS '.$_tabela);
+    }
   }
 
 
--- php/index.php
+++ php/index.php
@@ -3,5 +3,6 @@ require (__DIR__.DIRECTORY_SEPARATOR.'autoload.php');
 
 use classes\Banco;
 
-$ambiente = new Banco('teste');
+$ambiente = new Banco('localhost','root','');
 $ambiente->create_banco();
+$ambiente->conectar();
